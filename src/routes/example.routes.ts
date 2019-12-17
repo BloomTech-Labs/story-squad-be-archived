d91fed74ca0eb50aa7ba87c2abbbbeeecd25c118
William Sedlacek <wsedlacekc@gmail.com>
2019-12-17 17:21:42 -0600

    TypeORM Example
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/routes/example.routes.ts
+++ src/routes/example.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import { Example } from '../entity/Example';
 
 const exampleRoutes = Router();
 
-exampleRoutes.get('/', () => 'Hello World');
+exampleRoutes.get('/', (req, res) => res.send('Hello World'));
+exampleRoutes.post('/', (req, res) => {
+    getRepository(Example).save({ id: 1, name: 'test', isComplete: false });
+});
 
 export { exampleRoutes };
