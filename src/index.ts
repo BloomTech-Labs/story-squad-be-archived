7e58bd8d2bbfbf109d9f7c6e49992c87282bb1ea
William Sedlacek <wsedlacekc@gmail.com>
2019-12-17 17:21:42 -0600

    TypeORM Example
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import * as express from 'express';
 
+import { createConnection } from 'typeorm';
 import { exampleRoutes } from './routes/example.routes';
 
-const app = express();
-app.use(exampleRoutes);
+createConnection().then(() => {
+    const app = express();
+    app.use(exampleRoutes);
 
-app.listen(4000);
+    app.listen(4000);
+    console.log(`Listening on port ${4000}`);
+});
--- src/models/Middleware.ts
+++ src/models/Middleware.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import * as express from 'express';
+
 export type Middleware<T = {}> = (
     options?: T
 ) => (req: express.Request, res: express.Response, next: express.NextFunction) => void;
