791229a1f724ee6f1507c801c17157d2c46912d0
William Sedlacek <wsedlacekc@gmail.com>
2019-12-18 16:04:02 -0600

    Setup Auth Routes
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,17 +1,30 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
 
-import { RegisterDTO } from '../models/register.dto';
 import { Parent } from '../entity/Parent';
+import { Hash, ValidateHash } from '../middleware';
 
 const authRoutes = Router();
 
-authRoutes.post('/register', (req, res) => {
-    const user: RegisterDTO = req.body;
+authRoutes.use('/register', Hash());
+authRoutes.post('/register', async (req, res) => {
+    try {
+        const { password, ...user } = await getRepository(Parent).save(req.register);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json(err.toString());
+    }
+});
 
-    getRepository(Parent).save({ id: 1, name: 'test', isComplete: false });
-
-    res.json({ message: 'Hello' });
+authRoutes.use('/login', ValidateHash());
+authRoutes.use('/login', async (req, res) => {
+    try {
+        const token = sign(req.user, process.env.SECRET_SIGNATURE);
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json(err.toString());
+    }
 });
 
 export { authRoutes };
