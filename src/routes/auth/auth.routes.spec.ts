75bb2274f7d0023b4b23990870fa3ad83a123a2b
William Sedlacek <wsedlacekc@gmail.com>
2019-12-18 16:57:07 -0600

    Auth Routes Testing - WIP
    Start working on testing with Auth Routes.
    Currently these test are not passing due to typeorm configurations, I believe something like crossenv will be needed to properly set this up.
    
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/routes/auth/auth.routes.spec.ts
+++ src/routes/auth/auth.routes.spec.ts
@@ -1,33 +1,44 @@
 import * as request from 'supertest';
 import * as express from 'express';
 
-import * as typeorm from 'typeorm';
-import * as entity from '../entity/Example';
+import { getRepository, createConnection } from 'typeorm';
 
-jest.mock('typeorm', () => ({
-    getRepository: () => ({
-        find: () => true,
-        save: () => true,
-    }),
-}));
-
-jest.mock('../entity/Example', () => ({
-    Example: true,
-}));
-
-import { exampleRoutes } from './example.routes';
+import { globalMiddleware } from '../../middleware';
+import { Parent } from '../../database/entity/Parent';
+import { authRoutes } from './auth.routes';
 
 const app = express();
-app.use(exampleRoutes);
+globalMiddleware(app);
+app.use('/auth', authRoutes);
 
-test('GET should return 200 Okay', async () => {
-    await request(app)
-        .get('/')
-        .expect(200);
+beforeAll(async () => {
+    await createConnection('testing');
 });
 
-test('POST should return 201 Created', async () => {
-    await request(app)
-        .post('/')
-        .expect(201);
+beforeEach(async () => {
+    const repo = getRepository(Parent, 'testing');
+    await repo.clear();
+
+    const seed = new Parent();
+    seed.username = 'Test@gmail.com';
+    seed.password = '$2b$10$Hb9cjpL3DQGfn37705j5gu3vHJfgiTuS15U45NvYJhNzmoOmm/OdS';
+    await repo.save(seed);
+});
+
+describe('POST /login', () => {
+    it('should return 200 Okay', async () => {
+        await request(app)
+            .post('/auth/login')
+            .send({ username: 'Test@gmail.com', password: 'Test1234' })
+            .expect(200);
+    });
+});
+
+describe('POST /register', () => {
+    it('POST should return 201 Created when registration is completed', async () => {
+        await request(app)
+            .post('/auth/register')
+            .send({ username: 'NewTest@gmail.com', password: 'Test1234' })
+            .expect(201);
+    });
 });
