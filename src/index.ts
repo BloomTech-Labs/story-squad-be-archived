3333160cece29a93c50a875baae1e904538df039
ericbanker12 <ericbanker12@gmail.com>
2019-12-19 17:11:20 -0600

    add POST to Cannon routes

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import * as express from 'express';
 
 import { createConnection } from 'typeorm';
 import { exampleRoutes } from './routes/example.routes';
+import { cannonRoutes } from './routes/cannon/cannon.routes';
 
 createConnection().then(() => {
     const app = express();
+    app.use(express.json());
     app.use(exampleRoutes);
+    app.use('/cannon', cannonRoutes);
 
     app.listen(4000);
     console.log(`Listening on port ${4000}`);
