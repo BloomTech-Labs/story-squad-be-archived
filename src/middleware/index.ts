9c9b61159cb9d640b102562ea21175655ffbee23
William Sedlacek <wsedlacekc@gmail.com>
2019-12-20 12:15:18 -0600

    Enable CORS
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,11 @@
 import { Express, json } from 'express';
+import * as cors from 'cors';
+
 import { Validation } from './validate/validate.middleware';
 
 const globalMiddleware = (app: Express) => {
     app.use(json());
+    app.use(cors({ origin: '*' }));
     app.use(Validation());
 };
 
