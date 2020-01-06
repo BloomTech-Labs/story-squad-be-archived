a579cc64c5369f6e89a95b894d4f5ca06497c839
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-06 15:46:45 -0600

    imported childrenRoutes into the server

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import * as express from 'express';
 
 import { createConnection } from 'typeorm';
 import { exampleRoutes } from './routes/example.routes';
+import { childrenRoutes } from './routes/children/children.routes';
 
 createConnection().then(() => {
     const app = express();
     app.use(exampleRoutes);
+    app.use(childrenRoutes);
 
     app.listen(4000);
     console.log(`Listening on port ${4000}`);
--- src/routes/children/children.routes.ts
+++ src/routes/children/children.routes.ts
@@ -64,3 +64,5 @@ childrenRoutes.delete('/id', async (req, res) => {
         res.status(500).json(error.toString());
     }
 });
+
+export { childrenRoutes };
