69245bb49317eb14fbc317a7106ad7f97a93791d
William Sedlacek <wsedlacekc@gmail.com>
2020-01-07 15:07:23 -0600

    Fix Testing after variable name change

--- src/middleware/validate/validate.middleware.spec.ts
+++ src/middleware/validate/validate.middleware.spec.ts
@@ -1,10 +1,10 @@
 import { Validation } from './validate.middleware';
 
 describe('Validation()', () => {
-    it('should return a error if username is not an email', async () => {
+    it('should return a error if email is not an email', async () => {
         const req: any = {
             path: '/auth/register',
-            body: { username: 'test', password: 'Test1234', termsOfService: true },
+            body: { email: 'test', password: 'Test1234', termsOfService: true },
         };
         const res: any = {
             json: jest.fn(),
@@ -16,13 +16,13 @@ describe('Validation()', () => {
 
         await Validation()(req, res, next);
         expect(res.json).toHaveBeenCalled();
-        expect(res.json).toHaveBeenCalledWith({ errors: ['username must be an email'] });
+        expect(res.json).toHaveBeenCalledWith({ errors: ['email must be an email'] });
     });
 
     it('should return a error if termsOfService is not a boolean', async () => {
         const req: any = {
             path: '/auth/register',
-            body: { username: 'Test@mail.com', password: 'Test1234', termsOfService: '' },
+            body: { email: 'Test@mail.com', password: 'Test1234', termsOfService: '' },
         };
         const res: any = {
             json: jest.fn(),
@@ -42,7 +42,7 @@ describe('Validation()', () => {
     it('should return a error if password is not at least 8 characters', async () => {
         const req: any = {
             path: '/auth/register',
-            body: { username: 'Test@mail.com', password: 'Test123', termsOfService: true },
+            body: { email: 'Test@mail.com', password: 'Test123', termsOfService: true },
         };
         const res: any = {
             json: jest.fn(),
