4bf120878575637006094668d57581bace42253e
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-08 12:48:15 -0600

    changed error handling as per request; moved customer creation to auth routes

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -1,20 +1,32 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
+import * as Stripe from 'stripe';
 
 import { Parent } from '../../database/entity/Parent';
 import { Hash, ValidateHash } from '../../middleware';
 import { connection } from '../../util/typeorm-connection';
 
 const authRoutes = Router();
+const stripe = new Stripe('sk_test_v666XmnGJcP1Oz3GBg2iFmvd004Q3qp4jZ');
 
 authRoutes.use('/register', Hash());
 authRoutes.post('/register', async (req, res) => {
     try {
         if (!req.register.termsOfService)
             throw new Error('401: Accepting Terms of Service is Required');
+
         const { id } = await getRepository(Parent, connection()).save(req.register);
         const token = sign({ id }, process.env.SECRET_SIGNATURE || 'secret');
+
+        const customer = await stripe.customers.create({
+            email: req.body.token.email,
+        });
+
+        const parent = await getRepository(Parent).update(req.body.token.id, {
+            stripeId: customer.id,
+        });
+
         res.status(201).json({ token });
     } catch (err) {
         if (err.toString().includes('401')) res.status(401).json(err.toString());
--- src/routes/stripe/stripe.routes.ts
+++ src/routes/stripe/stripe.routes.ts
@@ -9,25 +9,6 @@ import { CheckJwt } from '../../middleware';
 const stripeRoutes = Router();
 const stripe = new Stripe('sk_test_v666XmnGJcP1Oz3GBg2iFmvd004Q3qp4jZ');
 
-// ADD CUSTOMER
-stripeRoutes.use('/new-customer', CheckJwt());
-stripeRoutes.post('/new-customer', async (req, res) => {
-    try {
-        const customer = await stripe.customers.create({
-            email: req.body.token.email,
-        });
-        const parent = await getRepository(Parent).update(req.body.token.id, {
-            stripeId: customer.id,
-        });
-
-        parent
-            ? res.status(201).json({ message: 'parent successfully registered as customer' })
-            : res.status(404).json({ message: 'could not find parent account' });
-    } catch (err) {
-        res.status(500).json(err.toString());
-    }
-});
-
 // ADD CARD
 stripeRoutes.use('/add-card', CheckJwt());
 stripeRoutes.put('/add-card', async (req, res) => {
@@ -35,11 +16,12 @@ stripeRoutes.put('/add-card', async (req, res) => {
         const customer = await stripe.customers.update(req.customer.id, {
             source: req.customer.sources.object,
         });
-        customer
-            ? res.status(201).json({ message: 'successfully added payment information' })
-            : res.status(404).json({ message: 'could not update payment information' });
+        if (!customer) throw new Error('404');
+        res.status(201).json({ message: 'successfully added payment information' });
     } catch (err) {
-        res.status(500).json(err.toString());
+        if (err == '404') {
+            res.status(404).json({ message: 'could not update payment information' });
+        } else res.status(500).json(err.toString());
     }
 });
 
@@ -53,11 +35,12 @@ stripeRoutes.use('/subscribe', async (req, res) => {
             items: [{ plan: 'plan_GVQ796LiwZugJ9' }],
             expand: ['latest_invoice.payment_intent'],
         });
-        subscription
-            ? res.status(201).json({ message: 'successfully subscribed' })
-            : res.status(404).json({ message: 'could not process subscription' });
+        if (!subscription) throw new Error('404');
+        res.status(201).json({ message: 'successfully subscribed' });
     } catch (err) {
-        res.status(500).json(err.toString());
+        if (err == '404') {
+            res.status(404).json({ message: 'could not process subscription' });
+        } else res.status(500).json(err.toString());
     }
 });
 
