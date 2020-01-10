19d762dc4cc5ea08261cddbd78a8203b29f8ea35
William Sedlacek <wsedlacekc@gmail.com>
2020-01-10 15:33:29 -0600

    Setup Stripe Test
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/routes/stripe/stripe.routes.ts
+++ src/routes/stripe/stripe.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import * as Stripe from 'stripe';
 
-import { Only } from '../../middleware';
+import { CardDTO } from '../../models';
 import { Parent } from '../../database/entity';
+import { Only } from '../../middleware';
 
 const stripe = new Stripe(process.env.STRIPE_API || 'sk_test_v666XmnGJcP1Oz3GBg2iFmvd004Q3qp4jZ');
 const stripeRoutes = Router();
@@ -10,8 +11,10 @@ const stripeRoutes = Router();
 stripeRoutes.get('/cards', Only(Parent), async (req, res) => {
     try {
         const user = req.user as Parent;
+
         const { data } = await stripe.customers.listSources(user.stripeID, { object: 'card' });
-        const cards = data.map(({ id, name, brand, last4, exp_month, exp_year }) => ({
+
+        const cards: CardDTO[] = data.map(({ id, name, brand, last4, exp_month, exp_year }) => ({
             id,
             name,
             brand,
@@ -28,7 +31,7 @@ stripeRoutes.get('/cards', Only(Parent), async (req, res) => {
 stripeRoutes.post('/cards', Only(Parent), async (req, res) => {
     try {
         const user = req.user as Parent;
-        await stripe.customers.createSource(user.stripeID, { source: req.card.id });
+        await stripe.customers.createSource(user.stripeID, { source: req.addCard.id });
         res.status(201).json({ message: 'Successfully added payment information' });
     } catch (err) {
         res.status(500).json({ message: 'Could not update payment information' });
@@ -39,14 +42,14 @@ stripeRoutes.delete('/cards/:id', Only(Parent), async (req, res) => {
     try {
         const user = req.user as Parent;
         await stripe.customers.deleteSource(user.stripeID, req.params.id);
-        res.status(201).json({ message: 'Successfully deleted payment information' });
+        res.json({ message: 'Successfully deleted payment information' });
     } catch (err) {
         res.status(500).json({ message: 'Could not delete payment information' });
     }
 });
 
 // The plan id used here is for the 'test plan' in our Stripe account
-stripeRoutes.use('/subscribe', Only(Parent), async (req, res) => {
+stripeRoutes.post('/subscribe', Only(Parent), async (req, res) => {
     try {
         const user = req.user as Parent;
         await stripe.customers.createSubscription(user.stripeID, {
