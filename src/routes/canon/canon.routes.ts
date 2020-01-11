353b35645962a7449ea3b3f041ba9db520f10104
William Sedlacek <wsedlacekc@gmail.com>
2020-01-10 19:16:08 -0600

    Correct Typo
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/routes/canon/canon.routes.ts
+++ src/routes/canon/canon.routes.ts
@@ -8,9 +8,9 @@ const canonRoutes = Router();
 
 canonRoutes.get('/:id', async (req, res) => {
     try {
-        const cannon = await getRepository(Canon, connection()).findOne(req.params.id);
-        if (!cannon) throw new Error('404');
-        res.json({ cannon });
+        const canon = await getRepository(Canon, connection()).findOne(req.params.id);
+        if (!canon) throw new Error('404');
+        res.json({ canon });
     } catch (err) {
         if (err.toString() === 'Error: 404')
             res.status(404).json(`chapter ${req.params.id} not found`);
@@ -21,8 +21,8 @@ canonRoutes.get('/:id', async (req, res) => {
 canonRoutes.post('/', async (req, res) => {
     // TODO: Add admin restriction
     try {
-        const cannon = await getRepository(Canon, connection()).save(req.addCanon);
-        res.status(201).json({ cannon });
+        const canon = await getRepository(Canon, connection()).save(req.addCanon);
+        res.status(201).json({ canon });
     } catch (err) {
         res.status(500).json(err.toString());
     }
