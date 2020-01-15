f5fe1fc69b63b044f90f725644efb6bae1a82752
ericbanker12 <ericbanker12@gmail.com>
2020-01-15 13:03:02 -0600

    add login and register endpoints to admin routes

--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -1,5 +1,8 @@
+import { hash, compare } from 'bcryptjs';
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import * as passGen from 'secure-random-password';
 
 import { connection } from '../../util/typeorm-connection';
 import { Admin } from '../../database/entity';
@@ -28,7 +31,40 @@ adminRoutes.get('/:id', async (req, res) => {
     }
 });
 
-adminRoutes.post('/', async (req, res) => {});
+adminRoutes.post('/login', async (req, res) => {
+    try {
+        const user: { email: string; password: string } = req.body;
+
+        const data = await getRepository(Admin, connection()).findOne({ email: user.email });
+        if (!data) throw Error('401');
+
+        if (!(await compare(user.password, data.password))) throw Error('401');
+
+        const token = sign({ adminID: data.id }, process.env.SECRET_SIGNATURE || 'secret');
+
+        res.status(200).json({ token });
+    } catch (err) {
+        if (err.toString().includes('401'))
+            res.status(401).json({ error: 'Failed to login, check your username and password...' });
+        else res.status(500).json(err.toString());
+    }
+});
+
+adminRoutes.post('/register', async (req, res) => {
+    try {
+        const user: { email: string; admin: boolean } = req.body;
+
+        // TO DO: replace with secure rng
+        const chars = '!#%+23456789:=?@ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+        const pass = passGen.randomPassword({ length: 16, characters: chars });
+
+        // const data = await getRepository(Admin, connection()).save({ email: user.email });
+
+        // res.status(201).json({ admin: data });
+    } catch (err) {
+        res.status(500).json(err.toString());
+    }
+});
 
 adminRoutes.put('/', async (req, res) => {});
 
