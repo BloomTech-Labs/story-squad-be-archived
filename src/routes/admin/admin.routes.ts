dc79240a88fe949feb6a4a47a8eed23692b80ed9
ericbanker12 <ericbanker12@gmail.com>
2020-01-15 18:00:23 -0600

    add default admin login and complete register logic

--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -35,14 +35,30 @@ adminRoutes.post('/login', async (req, res) => {
     try {
         const user: { email: string; password: string } = req.body;
 
-        const data = await getRepository(Admin, connection()).findOne({ email: user.email });
-        if (!data) throw Error('401');
+        if (user.email === 'admin' && user.password) {
+            // create default admin if no admins
+            const adminList = await getRepository(Admin, connection()).find({ super: true });
+            if (!adminList.length) {
+                const salt: number = parseInt(process.env.SALT || '3', 10);
+                const password = await hash(user.password, salt);
+                const data = await getRepository(Admin, connection()).save({
+                    ...user,
+                    password,
+                    super: true,
+                    validpass: true,
+                });
+                const token = sign({ adminID: data.id }, process.env.SECRET_SIGNATURE || 'secret');
+                res.status(200).json({ token });
+            }
+        } else {
+            const data = await getRepository(Admin, connection()).findOne({ email: user.email });
+            if (!data) throw Error('401');
 
-        if (!(await compare(user.password, data.password))) throw Error('401');
+            if (!(await compare(user.password, data.password))) throw Error('401');
 
-        const token = sign({ adminID: data.id }, process.env.SECRET_SIGNATURE || 'secret');
-
-        res.status(200).json({ token });
+            const token = sign({ adminID: data.id }, process.env.SECRET_SIGNATURE || 'secret');
+            res.status(200).json({ token });
+        }
     } catch (err) {
         if (err.toString().includes('401'))
             res.status(401).json({ error: 'Failed to login, check your username and password...' });
@@ -54,19 +70,25 @@ adminRoutes.post('/register', async (req, res) => {
     try {
         const user: { email: string; admin: boolean } = req.body;
 
-        // TO DO: replace with secure rng
         const chars = '!#%+23456789:=?@ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
         const pass = passGen.randomPassword({ length: 16, characters: chars });
+        const salt: number = parseInt(process.env.SALT || '3', 10);
+        const password = await hash(pass, salt);
 
-        // const data = await getRepository(Admin, connection()).save({ email: user.email });
+        const data = await getRepository(Admin, connection()).save({
+            email: user.email,
+            password,
+            validpass: false,
+            super: user.admin,
+        });
 
-        // res.status(201).json({ admin: data });
+        res.status(201).json({ admin: { ...data, password: pass } });
     } catch (err) {
         res.status(500).json(err.toString());
     }
 });
 
-adminRoutes.put('/', async (req, res) => {});
+adminRoutes.put('/:id', async (req, res) => {});
 
 adminRoutes.delete('/:id', async (req, res) => {});
 
