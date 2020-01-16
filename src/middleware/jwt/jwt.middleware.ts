e3157f4b6ea7dbebf26ff854eb7333ded8cad898
ericbanker12 <ericbanker12@gmail.com>
2020-01-15 19:02:25 -0600

    WIP add admin to jwt and only middleware

--- src/middleware/jwt/jwt.middleware.ts
+++ src/middleware/jwt/jwt.middleware.ts
@@ -2,20 +2,24 @@ import { verify } from 'jsonwebtoken';
 import { getRepository } from 'typeorm';
 
 import { Middleware, JWT } from '../../models';
-import { Child, Parent } from '../../database/entity';
+import { Child, Parent, Admin } from '../../database/entity';
 import { connection } from '../../util/typeorm-connection';
 
 const CheckJwt: Middleware = () => async (req, res, next) => {
     try {
         const parentRepo = getRepository(Parent, connection());
         const childRepo = getRepository(Child, connection());
+        const adminRepo = getRepository(Admin, connection());
 
-        const { parentID, childID } = verify(
+        const { parentID, childID, adminID } = verify(
             req.headers.authorization.replace('Bearer ', ''),
             process.env.SECRET_SIGNATURE || 'secret'
         ) as JWT;
 
-        req.user = childID
+        // To Do: fix error Type 'Parent | Child | Admin' not assignable to Type 'Parent | Child'
+        req.user = adminID
+            ? await adminRepo.findOne(adminID)
+            : childID
             ? await childRepo.findOne(childID, {
                   relations: ['parent'],
               })
