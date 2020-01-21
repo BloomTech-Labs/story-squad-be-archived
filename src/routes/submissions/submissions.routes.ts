8fd7828091749b64a2ef1bccb3ac3f26f8ce2af0
ericbanker12 <ericbanker12@gmail.com>
2020-01-21 16:17:05 -0600

    add PUT and DELETE to submission routes

--- src/routes/submissions/submissions.routes.ts
+++ src/routes/submissions/submissions.routes.ts
@@ -40,3 +40,41 @@ submissionRoutes.post('/', Only(Child), async (req, res) => {
         res.status(500).json({ message: 'Hmm... That did not work, please try again later.' });
     }
 });
+
+submissionRoutes.put('/:id', Only(Child), async (req, res) => {
+    try {
+        // To Do: add DTO to validation and set res.locals.submission
+        const submissionDTO = req.body as Submissions;
+        const id = parseInt(req.params.id);
+
+        const { submissions } = req.user as Child;
+        const oldSubmission = submissions.find(({ id }) => id === parseInt(req.params.id));
+        if (!oldSubmission) throw Error('404');
+
+        const submission = await getRepository(Submissions, connection()).update(id, submissionDTO);
+        res.json({ submission });
+    } catch (err) {
+        if (err.toString() === 'Error: 404')
+            res.status(404).json({ message: `Submission not found` });
+        else res.status(500).json({ message: 'Hmm... That did not work, please try again later.' });
+    }
+});
+
+submissionRoutes.delete('/:id', Only(Child), async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        const { submissions } = req.user as Child;
+        const submission = submissions.find(({ id }) => id === parseInt(req.params.id));
+        if (!submission) throw Error('404');
+
+        const { affected } = await getRepository(Submissions, connection()).delete(id);
+        if (!affected) throw Error();
+
+        res.json({ submission });
+    } catch (err) {
+        if (err.toString() === 'Error: 404')
+            res.status(404).json({ message: `Submission not found` });
+        else res.status(500).json({ message: 'Hmm... That did not work, please try again later.' });
+    }
+});
