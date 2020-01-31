e4093ac0e7a528bf2eea18024d6189fec12d3ade
William Sedlacek <wsedlacekc@gmail.com>
2020-01-31 13:19:23 -0600

    Use Jest for Node.js env
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'node',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
