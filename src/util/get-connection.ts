7b15480f38011c743bab5d236d6c66d56155393c
William Sedlacek <wsedlacekc@gmail.com>
2020-02-05 23:55:37 -0600

    Remove unused files
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/util/get-connection.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from 'dotenv';
-import * as PostgressConnectionStringParser from 'pg-connection-string';
-
-dotenv.config();
-
-const connectionOptions = PostgressConnectionStringParser.parse(process.env.DATABASE_URL || '');
-
-console.log(connectionOptions);
