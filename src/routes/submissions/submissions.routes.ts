f70a55c8f90c2c1893ee7467efc87e4185ff5a19
Hui <jeffrey.x.hui@gmail.com>
2020-03-09 13:58:38 -0700

    added readability persistence

--- src/routes/submissions/submissions.routes.ts
+++ src/routes/submissions/submissions.routes.ts
@@ -60,6 +60,8 @@ submissionRoutes.post('/', Only(Child), async (req, res) => {
             return res.status(400).json({ message: 'Something went wrong transcribing image.' });
         }
 
+        let readabilityStats = [];
+
         transcribed.images.forEach((story: string) => {
             readable({ story })
                 .then((stats: Readability) => {
@@ -70,6 +72,7 @@ submissionRoutes.post('/', Only(Child), async (req, res) => {
                     //     transcribed_text: story
                     // })
                     console.log(stats);
+                    readabilityStats.push(stats);
                     return res.status(200).json(stats);
                 })
                 .catch(console.error);
@@ -84,6 +87,24 @@ submissionRoutes.post('/', Only(Child), async (req, res) => {
             storyText,
             illustration,
             child: req.user,
+            // Readability
+            flesch_reading_ease: readabilityStats[0].flesch_reading_ease,
+            smog_index: readabilityStats[0].smog_index,
+            flesch_kincaid_grade: readabilityStats[0].flesch_kincaid_grade,
+            coleman_liau_index: readabilityStats[0].coleman_liau_index,
+            automated_readability_index: readabilityStats[0].automated_readability_index,
+            dale_chall_readability_score: readabilityStats[0].dale_chall_readability_score,
+            difficult_words: readabilityStats[0].difficult_words,
+            // needs to be a sum
+            linsear_write_formula: readabilityStats[0].linsear_write_formula,
+            gunning_fog: readabilityStats[0].gunning_fog,
+            consolidated_score: readabilityStats[0].consolidated_score,
+            doc_length: readabilityStats[0].doc_length,
+            // needs to be a sum
+            quote_count: readabilityStats[0].quote_count,
+            // needs to be a sum
+            transcribed_text: readabilityStats[0].transcribed_text,
+            // needs to be a concatenation
         });
         // END OLD DB CODE
 
