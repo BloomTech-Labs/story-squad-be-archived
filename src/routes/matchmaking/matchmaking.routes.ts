949dcfcda80a6700799c7971d50ec3db91859061
Mashima <52222646+zimashima@users.noreply.github.com>
2020-03-11 16:26:30 -0400

    fixed array issue in matchmaking

--- src/routes/matchmaking/matchmaking.routes.ts
+++ src/routes/matchmaking/matchmaking.routes.ts
@@ -60,13 +60,13 @@ matchMakingRoutes.get('/', Only(Admin), async (req, res) => {
         const competitions = await match(matchmaking)
         console.log(competitions[0])
         try{
-            for (let [key, value] of Object.entries(competitions)) {
+            for (let [key, value] of Object.entries(competitions[0])) {
                 console.log(`${key} and ${value}`)
                 await getRepository(Matches, connection()).save({
-                    team1_child1_id: parseInt(value.team_1[0]),
-                    team1_child2_id: parseInt(value.team_1[1]),
-                    team2_child1_id: parseInt(value.team_2[0]),
-                    team2_child2_id: parseInt(value.team_2[1]),
+                    team1_child1_id: parseInt(value['team_1'][0]),
+                    team1_child2_id: parseInt(value['team_1'][1]),
+                    team2_child1_id: parseInt(value['team_2'][0]),
+                    team2_child2_id: parseInt(value['team_2'][1]),
                     week: 0
                 })  
             }
