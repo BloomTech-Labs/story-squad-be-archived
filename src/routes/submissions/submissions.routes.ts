f4f799f65246ce594e0238cc3dd1741097a9ec78
Hui <jeffrey.x.hui@gmail.com>
2020-03-11 17:11:35 -0700

    adjusted route for entity change

--- src/routes/submissions/submissions.routes.ts
+++ src/routes/submissions/submissions.routes.ts
@@ -79,11 +79,11 @@ submissionRoutes.post('/', Only(Child), async (req, res) => {
 
                 ...readabilityStats[0],
                 transcribed_text: {
-                    page1: transcribed.images[0] ? transcribed.images[0] : '',
-                    page2: transcribed.images[1] ? transcribed.images[1] : '',
-                    page3: transcribed.images[2] ? transcribed.images[2] : '',
-                    page4: transcribed.images[3] ? transcribed.images[3] : '',
-                    page5: transcribed.images[4] ? transcribed.images[4] : '',
+                    t_page1: transcribed.images[0] ? transcribed.images[0] : '',
+                    t_page2: transcribed.images[1] ? transcribed.images[1] : '',
+                    t_page3: transcribed.images[2] ? transcribed.images[2] : '',
+                    t_page4: transcribed.images[3] ? transcribed.images[3] : '',
+                    t_page5: transcribed.images[4] ? transcribed.images[4] : '',
                 },
             });
 
