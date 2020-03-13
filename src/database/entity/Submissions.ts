dbe6eb57ae802c68a636ba1c7d141e4cc3b71101
Hui <jeffrey.x.hui@gmail.com>
2020-03-13 12:01:35 -0700

    added match check in matchmaking routes so it returns pre-existing generated matches if selected week already has them

--- src/database/entity/Submissions.ts
+++ src/database/entity/Submissions.ts
@@ -29,9 +29,6 @@ class Submissions {
 
     @Column({ nullable: true })
     illustration: string;
-    
-    @Column({ nullable: true })
-    type: 'story' | 'illustration';
 
     @Column({ nullable: true })
     type: 'story' | 'illustration';
