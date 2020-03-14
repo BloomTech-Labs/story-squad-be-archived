f2af159072948cd5ef6d89461ca67106625a3f3d
Hui <jeffrey.x.hui@gmail.com>
2020-03-14 12:32:53 -0700

    updated activites interface

--- src/models/internal/Activities.ts
+++ src/models/internal/Activities.ts
@@ -2,9 +2,9 @@ interface Activities {
     reading: any;
     writing: any;
     drawing: any;
-    // teamReview: any;
-    // randomReview: any;
-    // results: any;
+    teamReview: any;
+    randomReview: any;
+    results: any;
 }
 
 export { Activities };
