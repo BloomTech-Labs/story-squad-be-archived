fd1267326034795a8555f4ae46225d4d4ed0e1b8
Hui <jeffrey.x.hui@gmail.com>
2020-03-19 17:30:12 -0700

    remove merge conflict in submissions

--- src/routes/battles/custom.ts
+++ src/routes/battles/custom.ts
@@ -1,72 +1,68 @@
-import { EntityRepository, EntityManager } from 'typeorm'
+// import { EntityRepository, EntityManager } from 'typeorm'
 
-import { Child, Submissions } from '../../database/entity';
+// import { Child } from '../../database/entity';
 
-@EntityRepository()
-export class MatchInfoRepository {
-    constructor(private manager: EntityManager) {}
+// @EntityRepository()
+// export class MatchInfoRepository {
+//     constructor(private manager: EntityManager) {}
 
-    findMatchInfo(student1id: number, student2id: number, week: number) {
-        let submissions = null;
-        let student1 = {
-            id: student1id,
-            story: {},
-            illustration: {},
-            username: "",
-            avatar: "",
-        };
-        let student2 = {
-            id: student2id,
-            story: {},
-            illustration: {},
-            username: "",
-            avatar: "",
-        };
-        return this.manager
-            .find(Submissions, {
-                where: { week: week },
-            })
-            .then((res) => {
-                submissions = res;
-                console.log(submissions)
-                student1.story = submissions.filter(
-                    (submission) => submission['childId'] === student1id
-                );
-                student1.illustration = submissions.filter(
-                    (submission) =>
-                        submission['childId'] === student1id
-                );
-                student2.story = submissions.filter(
-                    (submission) => submission['childId'] === student2id
-                );
-                student2.illustration = submissions.filter(
-                    (submission) =>
-                        submission['childId'] === student2id
-                )
-                
-                return this.manager.findOne(Child, {
-                    where: { id: student1id }
-                }).then(res => {
-                    student1.username = res.username
-                    student1.avatar = res.avatar
+//     findMatchInfo(student1id: number, student2id: number, week: number) {
+//         let submissions = null;
+//         let student1 = {
+//             id: student1id,
+//             story: {},
+//             illustration: {},
+//             username: "",
+//             avatar: "",
+//         };
+//         let student2 = {
+//             id: student2id,
+//             story: {},
+//             illustration: {},
+//             username: "",
+//             avatar: "",
+//         };
+//         return this.manager
+//             .find(Submissions, {
+//                 where: { week: week },
+//             })
+//             .then((res) => {
+//                 submissions = res;
+//                 console.log(submissions)
+//                 student1.story = submissions.filter(
+//                     (submission) => submission['childId'] === student1id
+//                 );
+//                 student1.illustration = submissions.filter(
+//                     (submission) =>
+//                         submission['childId'] === student1id
+//                 );
+//                 student2.story = submissions.filter(
+//                     (submission) => submission['childId'] === student2id
+//                 );
+//                 student2.illustration = submissions.filter(
+//                     (submission) =>
+//                         submission['childId'] === student2id
+//                 )
 
-                    return this.manager.findOne(Child, {
-                        where: { id: student2id }
-                    }).then(res => {
-                            student2.username = res.username
-                            student2.avatar = res.avatar
+//                 return this.manager.findOne(Child, {
+//                     where: { id: student1id }
+//                 }).then(res => {
+//                     student1.username = res.username
+//                     student1.avatar = res.avatar
 
-                            return { student1, student2 }
-                    })
-                        
-                })
+//                     return this.manager.findOne(Child, {
+//                         where: { id: student2id }
+//                     }).then(res => {
+//                             student2.username = res.username
+//                             student2.avatar = res.avatar
 
-                    
-            })
+//                             return { student1, student2 }
+//                     })
 
-                
-                
-        };
-        
-    }
+//                 })
 
+//             })
+
+//         };
+
+//     }
