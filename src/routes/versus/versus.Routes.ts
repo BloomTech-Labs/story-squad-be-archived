c4290569228a5745da3ea44f4efff475ac41962f
Young Won <youngwon@Youngs-MacBook-Pro.local>
2020-04-19 09:42:05 -0700

    refactored versusroutes

--- src/routes/versus/versus.Routes.ts
+++ src/routes/versus/versus.Routes.ts
@@ -7,6 +7,11 @@ import { Only } from '../../middleware/only/only.middleware';
 import { connection } from '../../util/typeorm-connection';
 import e = require('express');
 import { decideHigher, returnMatch } from './versusRoutes.functions';
+import { assignRole } from './assignRole';
+import { matchVersusPlayers } from './matchVersusPlayers';
+import { createTeam } from './createTeam';
+import { createStudent } from './createStudent';
+
 const versusRoutes = Router();
 
 versusRoutes.get('/versus', Only(Child), async (req, res) => {
@@ -23,7 +28,6 @@ versusRoutes.get('/versus', Only(Child), async (req, res) => {
         homeTeam.student.username = username;
         homeTeam.student.avatar = avatar;
 
-        let student_id = null;
         let teammate_id = null;
 
         //awayTeam created
@@ -59,108 +63,47 @@ versusRoutes.get('/versus', Only(Child), async (req, res) => {
 
             const [story] = stories.filter((el) => el.week === cohort.week);
             const [illustration] = illustrations.filter((el) => el.week === cohort.week);
-            homeTeam.student.story = story.story;
-            homeTeam.student.storyPoints = story.points;
-            homeTeam.student.illustration = illustration.illustration;
-            homeTeam.student.illustrationPoints = illustration.points;
-            homeTeam.student.role = 'student';
+
+            // creation for homeTeam student
+            homeTeam.student = createStudent(homeTeam, story, illustration);
 
             const teammate = await getCustomRepository(
                 MatchInfoRepository,
                 connection()
             ).findStudentInfo(teammate_id, cohort.week);
 
-            homeTeam.teammate = {
-                ...homeTeam.teammate,
-                ...teammate,
-                role: 'teammate',
-            };
+            // creation for homeTeam teammate
+            homeTeam.teammate = createTeam(homeTeam, teammate);
 
             const opponentA = await getCustomRepository(
                 MatchInfoRepository,
                 connection()
             ).findStudentInfo(opponentA_id, cohort.week);
 
-            awayTeam.student = {
-                ...awayTeam.student,
-                ...opponentA,
-                role: 'opponentA',
-            };
+            // creation for awayTeam student
+            awayTeam.student = createTeam(awayTeam, opponentA);
 
             const opponentB = await getCustomRepository(
                 MatchInfoRepository,
                 connection()
             ).findStudentInfo(opponentB_id, cohort.week);
-            // console.log('opponentB', awayTeam.opponentB.story);
 
-            awayTeam.teammate = {
-                ...awayTeam.teammate,
-                ...opponentB,
-                role: 'opponentB',
-            };
+            // creation for awayTeam teammate
+            awayTeam.teammate = createTeam(awayTeam, opponentB);
         }
-
-        // Round allocation functions below
-
+        // createTeam(awayTeam, opponentB);
+        // who is higher in story and illustration points between student and teammate in homeTeam?
         const higherMyteam = decideHigher(homeTeam.student, homeTeam.teammate);
-        // array of arrays returned by decideHighter  [[storypoint high, storypoint low], [illustrationpoint high, illustrationpoint low]]
-        if (higherMyteam[0][0].role === 'student' && higherMyteam[1][0].role === 'student') {
-            homeTeam.student.storyRole = 'storyHigh';
-            homeTeam.teammate.storyRole = 'storyLow';
-            homeTeam.student.illustrationRole = 'illustrationHigh';
-            homeTeam.teammate.illustrationRole = 'illustrationLow';
-        } else {
-            homeTeam.student.storyRole = 'storyLow';
-            homeTeam.teammate.storyRole = 'storyHigh';
-            homeTeam.student.illustrationRole = 'illustrationLow';
-            homeTeam.teammate.illustrationRole = 'illustrationHigh';
-        }
+        // assign storyRole and illustrationRole to student and teammate in homeTeam
+        assignRole(higherMyteam, homeTeam);
 
+        // who is higher in story and illustration points between opponentA and opponentB in awayTeam?
         const higherTeam2 = decideHigher(awayTeam.student, awayTeam.teammate);
+        // assign storyRole and illustrationRole to opponentA and opponentB in awayTeam
+        assignRole(higherTeam2, awayTeam);
 
-        if (higherTeam2[0][0].role === 'opponentA' && higherTeam2[1][0].role === 'opponentA') {
-            awayTeam.student.storyRole = 'storyHigh';
-            awayTeam.teammate.storyRole = 'storyLow';
-            awayTeam.student.illustrationRole = 'illustrationHigh';
-            awayTeam.teammate.illustrationRole = 'illustrationLow';
-        } else {
-            awayTeam.student.storyRole = 'storyLow';
-            awayTeam.teammate.storyRole = 'storyHigh';
-            awayTeam.student.illustrationRole = 'illustrationLow';
-            awayTeam.teammate.illustrationRole = 'illustrationHigh';
-        }
-
-        if (homeTeam.student.storyRole === awayTeam.student.storyRole) {
-            homeTeam.student.storyOpponent = awayTeam.student;
-            homeTeam.teammate.storyOpponent = awayTeam.teammate;
-            homeTeam.student.storyTotal =
-                homeTeam.student.storyPoints + awayTeam.student.storyPoints;
-            homeTeam.teammate.storyTotal =
-                homeTeam.teammate.storyPoints + awayTeam.teammate.storyPoints;
-        } else {
-            homeTeam.student.storyOpponent = awayTeam.teammate;
-            homeTeam.teammate.storyOpponent = awayTeam.student;
-            homeTeam.student.storyTotal =
-                homeTeam.student.storyPoints + awayTeam.teammate.storyPoints;
-            homeTeam.teammate.storyTotal =
-                homeTeam.teammate.storyPoints + awayTeam.student.storyPoints;
-        }
-
-        if (homeTeam.student.illustrationRole === awayTeam.student.illustrationRole) {
-            homeTeam.student.illustrationOpponent = awayTeam.student;
-            homeTeam.teammate.illustrationOpponent = awayTeam.teammate;
-            homeTeam.student.illustrationTotal =
-                homeTeam.student.illustrationPoints + awayTeam.student.illustrationPoints;
-            homeTeam.teammate.illustrationTotal =
-                homeTeam.teammate.illustrationPoints + awayTeam.teammate.illustrationPoints;
-        } else {
-            homeTeam.student.illustrationOpponent = awayTeam.teammate;
-            homeTeam.teammate.illustrationOpponent = awayTeam.student;
-            homeTeam.student.illustrationTotal =
-                homeTeam.student.illustrationPoints + awayTeam.teammate.illustrationPoints;
-            homeTeam.teammate.illustrationTotal =
-                homeTeam.teammate.illustrationPoints + awayTeam.student.illustrationPoints;
-        }
+        // matching players for student and teammate with players in opponent team
+        matchVersusPlayers(homeTeam, awayTeam);
 
         const thisBattle = {
             battleInfo: {
