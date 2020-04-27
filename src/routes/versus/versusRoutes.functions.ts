4ba708db9f42985a22ea573f8e77b4b3f26e6c8c
Logan Van Hook <loganvan@att.net>
2020-04-27 16:51:02 -0400

    Scrub files

--- src/routes/versus/versusRoutes.functions.ts
+++ src/routes/versus/versusRoutes.functions.ts
@@ -1,7 +1,3 @@
-import { Matches } from '../../database/entity';
-import { getRepository } from 'typeorm';
-import { connection } from '../../util/typeorm-connection';
-
 function decideHigher(studentA, studentB) {
     const high = [];
     if (studentA.storyPoints > studentB.storyPoints) high.push([studentA, studentB]);
@@ -13,32 +9,4 @@ function decideHigher(studentA, studentB) {
     return high;
 }
 
-async function returnMatch(id: number, week: number) {
-    const match = await getRepository(Matches, connection()).findOne({
-        where: [{ team1_child1_id: id, week: week }],
-    });
-
-    return match ? match : r2(id, week);
-}
-
-async function r2(id: number, week: number) {
-    const match = await getRepository(Matches, connection()).findOne({
-        where: [{ team1_child2_id: id, week: week }],
-    });
-    return match ? match : r3(id, week);
-}
-async function r3(id: number, week: number) {
-    const match = await getRepository(Matches, connection()).findOne({
-        where: [{ team2_child1_id: id, week: week }],
-    });
-    console.log(match);
-    return match ? match : r4(id, week);
-}
-async function r4(id: number, week: number) {
-    const match = await getRepository(Matches, connection()).findOne({
-        where: [{ team2_child2_id: id, week: week }],
-    });
-    console.log(match);
-    return match ? match : null;
-}
-export { decideHigher, returnMatch };
+export { decideHigher };
