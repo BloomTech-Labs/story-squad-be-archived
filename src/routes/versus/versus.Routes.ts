f1e043eaa15fa2a205602edb58f80d1a6b891c00
Logan Van Hook <loganvan@att.net>
2020-04-28 11:45:26 -0400

    Create imports file

--- src/routes/versus/versus.Routes.ts
+++ src/routes/versus/versus.Routes.ts
@@ -1,43 +1,20 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
-import { Child, Stories } from '../../database/entity';
+import { Child } from '../../database/entity';
 import { Only } from '../../middleware/only/only.middleware';
 import { connection } from '../../util/typeorm-connection';
-import e = require('express');
 import { TypeCast } from '../../util/utils';
 import { FindMatchByUID } from '../../util/db-utils';
-import { Transcribable } from '../../models';
+import { StorySend, LEFT, RIGHT } from './versusRoutes.imports';
 
 const versusRoutes = Router();
 
-class StorySend {
-    id: Number;
-    childId: Number;
-    username: String;
-    points: Number;
-    doc_length: Number;
-    story: Stories;
-    transcribed_text: Transcribable;
-
-    constructor() {
-        this.id = null;
-        this.childId = null;
-        this.username = null;
-        this.points = null;
-        this.doc_length = null;
-        this.story = null;
-        this.transcribed_text = null;
-    }
-}
-
-let LEFT = 0;
-let RIGHT = 1;
-
 versusRoutes.get('/versus', Only(Child), async (req, res) => {
     try {
-        const { id, cohort, username, avatar, stories, illustrations } = req.user as Child;
+        const { id, cohort, username, avatar, stories, illustrations, votes } = req.user as Child;
 
         const match = await FindMatchByUID(id, cohort.week);
+        let teamID = match.team1_child1_id === id || match.team1_child2_id === id ? 1 : 2;
 
         if (!match) {
             res.json(401).json({
@@ -57,6 +34,10 @@ versusRoutes.get('/versus', Only(Child), async (req, res) => {
             where: [{ id: match.team2_child1_id }, { id: match.team2_child2_id }],
             relations: ['illustrations', 'stories'],
         });
+
+        let matchdata = {
+            votes,
+        };
 
         //Extract stories, highest points to lowest
         const team1Stories = sortByPoints(team1, 'stories', cohort.week);
@@ -107,24 +88,28 @@ versusRoutes.get('/versus', Only(Child), async (req, res) => {
         ].illustration = 'DRAWING DRAWING DRAWING DRAWING';
         //////////////////////////////////////////////////
 
-        //Create battle data :)
-        let thisBattle = {
-            highStory: HighStoryMatchup,
-            lowStory: LowStoryMatchup,
-            highIll: HighIllustrationMatchup,
-            lowIll: LowIllustrationMatchup,
-        };
+        let thisBattle = [
+            HighStoryMatchup,
+            LowStoryMatchup,
+            HighIllustrationMatchup,
+            LowIllustrationMatchup,
+        ];
+
+        //Sort the battle, high points to low
+        thisBattle.sort(function (a, b) {
+            return b.points - a.points;
+        });
 
         //Resolve usernames & any other data we may have lost through the sorting process
         [team1[0], team1[1], team2[0], team2[1]].forEach((i) => {
             Object.keys(thisBattle).forEach((e) => {
-                let Resolve = thisBattle[e];
+                let Resolve = thisBattle[e] as StorySend[];
                 if (Resolve[0].id === i.id) Resolve[0].username = i.username;
                 else if (Resolve[1].id === i.id) Resolve[1].username = i.username;
             });
         });
 
-        return res.status(200).json(thisBattle);
+        return res.status(200).json({ matchdata: matchdata, matchups: thisBattle });
     } catch (err) {
         return res.status(500).json({ message: err.toString() });
     }
