6d82242c31a996c5a5e471b0b57c4023b0c2d238
Logan Van Hook <loganvan@att.net>
2020-05-01 18:24:15 -0400

    Find matches in cohort

--- src/timers/pointalloc.timers.ts
+++ src/timers/pointalloc.timers.ts
@@ -11,13 +11,49 @@ async function point_allocation_timer() {
         //Get current date
         let Current = new Date();
 
-        //For every cohort
+        //Get matches
+        let TotalMatches = await MatchesRepo.find();
+
+        //Get cohorts & their children
         let Cohorts = await CohortRepo.find({ relations: ['children'] });
 
+        //For every cohort
         await Cohorts.forEach(async (i) => {
             if (i.activity === 'teamReview')
                 if (Current > i.dueDates.teamReview) {
                     //Find matches from this cohort
+                    //This can be removed IF the matches get
+                    //team1: child[], team2: child[]
+                    //instead of literal number id's
+                    let CohortMatches = [];
+                    TotalMatches.forEach((e) => {
+                        i.children.forEach((x) => {
+                            if (CohortMatches.includes(e)) return;
+                            switch (x.id) {
+                                case e.team1_child1_id:
+                                    CohortMatches.push(e);
+                                    break;
+                                case e.team1_child2_id:
+                                    CohortMatches.push(e);
+                                    break;
+                                case e.team2_child1_id:
+                                    CohortMatches.push(e);
+                                    break;
+                                case e.team2_child2_id:
+                                    CohortMatches.push(e);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        });
+                    });
+
+                    //Check if children in each match have voted
+
+                    //For every match
+                    CohortMatches.forEach((match) => {
+                        //Check children
+                    });
 
                     //Set teamReview & save
                     i.activity = 'randomReview';
