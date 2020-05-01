d68c8734855a6be7a2b5712bd6633e1840665aca
Logan Van Hook <loganvan@att.net>
2020-05-01 17:59:14 -0400

    Add children to cohorts

--- src/timers/pointalloc.timers.ts
+++ src/timers/pointalloc.timers.ts
@@ -1,21 +1,23 @@
-import { Cohort } from '../database/entity';
+import { Cohort, Child, Matches } from '../database/entity';
 import { getRepository } from 'typeorm';
 import { connection } from '../util/typeorm-connection';
 
 async function point_allocation_timer() {
     setInterval(async function () {
         let CohortRepo = await getRepository(Cohort, connection());
+        let ChildRepo = await getRepository(Child, connection());
+        let MatchesRepo = await getRepository(Matches, connection());
 
         //Get current date
         let Current = new Date();
 
         //For every cohort
-        let Cohorts = await CohortRepo.find();
+        let Cohorts = await CohortRepo.find({ relations: ['children'] });
 
         await Cohorts.forEach(async (i) => {
             if (i.activity === 'teamReview')
                 if (Current > i.dueDates.teamReview) {
-                    //Do checks on everybody
+                    //Find matches from this cohort
 
                     //Set teamReview & save
                     i.activity = 'randomReview';
