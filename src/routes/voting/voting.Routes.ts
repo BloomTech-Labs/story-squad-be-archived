46f892a640ef2a3a69c2d0a56205ee8906bc4229
Logan Van Hook <loganvan@att.net>
2020-05-07 14:09:18 -0400

    Random match finder setup

--- src/routes/voting/voting.Routes.ts
+++ src/routes/voting/voting.Routes.ts
@@ -1,11 +1,33 @@
 import { Router } from 'express';
 import { Only } from '../../middleware';
 import { Child } from '../../database/entity';
+import { getManager, Not } from 'typeorm';
+import { connection } from '../../util/typeorm-connection';
+import { Versus } from '../../database/entity/Versus';
 
 const votingRoutes = Router();
 
+async function randomIgnoring(ignore: number) {
+    const manager = getManager(connection());
+
+    try {
+        let Response = await manager
+            .createQueryBuilder(Versus, 'versus')
+            .where({ id: Not(ignore) })
+            .orderBy('RANDOM()')
+            .limit(1)
+            .getOne();
+
+        return Response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 votingRoutes.get('/voting', Only(Child), async (req, res) => {
-    res.status(200).send('test');
+    let response = await randomIgnoring(5);
+
+    res.status(200).json(response);
 });
 
 export { votingRoutes };
