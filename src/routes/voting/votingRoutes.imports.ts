5ffd3c9c848da0201318cc5d2ad44e9f88e50466
Logan Van Hook <55011072+lgv-0@users.noreply.github.com>
2020-05-13 13:08:35 -0400

    Voting Hotfix (#126)
    * Fix ID returned from repo
    
    * HotFix

--- src/routes/voting/votingRoutes.imports.ts
+++ src/routes/voting/votingRoutes.imports.ts
@@ -2,23 +2,23 @@ import { Stories } from '../../database/entity';
 import { Transcribed_Pages } from '../../database/entity/Pages';
 
 export class illustrationReturn {
-    id: number;
+    childId: number;
     illustration: string;
 
     constructor() {
-        this.id = null;
+        this.childId = null;
         this.illustration = null;
     }
 }
 
 export class storyReturn {
-    id: number;
+    childId: number;
     doc_length: number;
     story: Stories;
     transcribed_text: Transcribed_Pages;
 
     constructor() {
-        this.id = null;
+        this.childId = null;
         this.doc_length = null;
         this.story = null;
         this.transcribed_text = null;
