292aba4e7c695568ddbaee07ddcc6a4dedc31835
Logan Van Hook <loganvan@att.net>
2020-05-13 12:39:59 -0400

    Fix ID returned from repo

--- src/routes/voting/votingRoutes.imports.ts
+++ src/routes/voting/votingRoutes.imports.ts
@@ -12,13 +12,13 @@ export class illustrationReturn {
 }
 
 export class storyReturn {
-    id: number;
+    childId: number;
     doc_length: number;
     story: Stories;
     transcribed_text: Transcribed_Pages;
 
     constructor() {
-        this.id = null;
+        this.childId = null;
         this.doc_length = null;
         this.story = null;
         this.transcribed_text = null;
