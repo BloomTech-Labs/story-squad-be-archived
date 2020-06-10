08ad3e91a2ce07c53ede63bdbb409c593d9217e9
JGPico <minq_2@hotmail.com>
2020-06-10 16:21:09 -0600

    Fully operational and automatic voting. results endpoint

--- src/routes/finalScreen/final.routes.ts
+++ src/routes/finalScreen/final.routes.ts
@@ -2,14 +2,15 @@ import { Router } from 'express';
 import { getRepository } from 'typeorm';
 
 import { connection } from '../../util/typeorm-connection';
-import { Child, Matches, Stories, Illustrations, Cohort } from '../../database/entity';
+import { Child, Stories, Illustrations } from '../../database/entity';
+import { Versus } from '../../database/entity/Versus';
 import { Only } from '../../middleware';
 
 import { FindMatchByUID } from '../../util/db-utils';
 
 const finalRoutes = Router();
 
-finalRoutes.get('/', Only(Child), async (req, res) => {
+finalRoutes.get('/time', Only(Child), async (req, res) => {
     try {
         const { cohort } = req.user as Child;
 
@@ -37,14 +38,13 @@ finalRoutes.get('/', Only(Child), async (req, res) => {
     }
 });
 
-finalRoutes.get('/results/', Only(Child), async (req, res) => {
+finalRoutes.get('/results', Only(Child), async (req, res) => {
     try {
         const { cohort, id } = req.user as Child;
         let ChildRepo = getRepository(Child, connection());
+        let VersusRepo = getRepository(Versus, connection());
         let StoryRepo = getRepository(Stories, connection());
         let IllustrationRepo = getRepository(Illustrations, connection());
-        let MatchesRepo = getRepository(Matches, connection());
-        let CohortRepo = getRepository(Cohort, connection());
 
         let ChildMatch = await FindMatchByUID(id, cohort.week);
 
@@ -54,58 +54,77 @@ finalRoutes.get('/results/', Only(Child), async (req, res) => {
             return;
         }
 
-        let Build = {} as any;
-
-        let StoryT1C1 = await StoryRepo.findOne({
-            where: [{ childId: ChildMatch.team1_child1_id, week: cohort.week }],
-        });
-        let StoryT1C2 = await StoryRepo.findOne({
-            where: [{ childId: ChildMatch.team1_child2_id, week: cohort.week }],
-        });
-        let StoryT2C1 = await StoryRepo.findOne({
-            where: [{ childId: ChildMatch.team2_child1_id, week: cohort.week }],
-        });
-        let StoryT2C2 = await StoryRepo.findOne({
-            where: [{ childId: ChildMatch.team2_child2_id, week: cohort.week }],
+        let VersusInMatch = await VersusRepo.find({
+            where: { match: ChildMatch.id },
         });
 
-        let PictureT1C1 = await IllustrationRepo.findOne({
-            where: [{ childId: ChildMatch.team1_child1_id, week: cohort.week }],
-        });
-        let PictureT1C2 = await IllustrationRepo.findOne({
-            where: [{ childId: ChildMatch.team1_child2_id, week: cohort.week }],
-        });
-        let PictureT2C1 = await IllustrationRepo.findOne({
-            where: [{ childId: ChildMatch.team2_child1_id, week: cohort.week }],
-        });
-        let PictureT2C2 = await IllustrationRepo.findOne({
-            where: [{ childId: ChildMatch.team2_child2_id, week: cohort.week }],
-        });
+        let V1 = {} as any;
+        V1.versus = 1;
+        let V2 = {} as any;
+        V2.versus = 2;
+        let V3 = {} as any;
+        V3.versus = 3;
+        let V4 = {} as any;
+        V4.versus = 4;
 
-        Build.StoryT1C1votes = StoryT1C1.votes;
-        Build.StoryT1C2votes = StoryT1C2.votes;
-        Build.StoryT2C1votes = StoryT2C1.votes;
-        Build.StoryT2C2votes = StoryT2C2.votes;
+        let Build = [V1, V2, V3, V4] as any;
 
-        Build.StoryT1C1points = StoryT1C1.points;
-        Build.StoryT1C2points = StoryT1C2.points;
-        Build.StoryT2C1points = StoryT2C1.points;
-        Build.StoryT2C2points = StoryT2C2.points;
+        // get stories and pics for one versus. Repeat x4
 
-        Build.PictureT1C1votes = PictureT1C1.votes;
-        Build.PictureT1C2votes = PictureT1C2.votes;
-        Build.PictureT2C1votes = PictureT2C1.votes;
-        Build.PictureT2C2votes = PictureT2C2.votes;
+        for (let j = 0; j < 4; j++) {
+            let singleVersus = VersusInMatch[j];
+            let versusC1 = singleVersus.children[0].id;
+            let C1Name = await ChildRepo.findOne({
+                where: { id: versusC1 },
+            });
+            Build[j].C1Name = C1Name.username;
+            let versusC2 = singleVersus.children[1].id;
+            let C2Name = await ChildRepo.findOne({
+                where: { id: versusC2 },
+            });
+            Build[j].C2Name = C2Name.username;
 
-        Build.PictureT1C1points = PictureT1C1.points;
-        Build.PictureT1C2points = PictureT1C2.points;
-        Build.PictureT2C1points = PictureT2C1.points;
-        Build.PictureT2C2points = PictureT2C2.points;
+            if (singleVersus.story) {
+                let StoryC1 = await StoryRepo.findOne({
+                    where: [{ childId: versusC1 }],
+                });
+                let StoryC2 = await StoryRepo.findOne({
+                    where: [{ childId: versusC2 }],
+                });
+                Build[j].C1Id = versusC1;
+                Build[j].C2Id = versusC2;
+                Build[j].StoryC1votes = StoryC1.votes;
+                Build[j].StoryC2votes = StoryC2.votes;
+                Build[j].totalPoints = StoryC1.points + StoryC2.points;
+                Build[j].winnerId = StoryC1.votes > StoryC2.votes ? versusC1 : versusC2;
+                Build[j].winnerName =
+                    StoryC1.votes > StoryC2.votes ? C1Name.username : C2Name.username;
+                Build[j].StoryC1points = StoryC1.points;
+                Build[j].StoryC2points = StoryC2.points;
+            } else {
+                // else its an illustration
+                let PictureC1 = await IllustrationRepo.findOne({
+                    where: [{ childId: versusC1 }],
+                });
+                let PictureC2 = await IllustrationRepo.findOne({
+                    where: [{ childId: versusC2 }],
+                });
+                Build[j].C1Id = versusC1;
+                Build[j].C2Id = versusC2;
+                Build[j].PictureC1votes = PictureC1.votes;
+                Build[j].PictureC2votes = PictureC2.votes;
+                Build[j].totalPoints = PictureC1.points + PictureC2.points;
+                Build[j].winnerId = PictureC1.votes > PictureC2.votes ? versusC1 : versusC2;
+                Build[j].winnerName =
+                    PictureC1.votes > PictureC2.votes ? C1Name.username : C2Name.username;
+                Build[j].PictureC1points = PictureC1.points;
+                Build[j].PictureC2points = PictureC2.points;
+            }
+        } // end for
 
         return res.status(200).json({
-            ChildMatch,
             Build,
-            StoryT2C2,
+            //VersusInMatch,
         });
     } catch (err) {
         console.log('error', err.toString());
--- src/timers/voting.timers.ts
+++ src/timers/voting.timers.ts
@@ -24,160 +24,49 @@ async function vote_allocation_timer() {
 
         await Cohorts.forEach(async (i) => {
             if (Current > i.dueDates.randomReview) {
-                //Find matches from this cohort
-                let MatchesInWeek = await MatchesRepo.find({
-                    where: [{ week: i.week }],
+                let VersusInCohort = await VersusRepo.find({
+                    where: { cohortId: i.id },
                 });
-                // we need verus sets from different matchups.
-                let CohortMatches = FindMatchesByChildren(MatchesInWeek, i.children) as Matches[];
 
-                //Check if children in each match have voted
-
-                //For every match
-                await CohortMatches.forEach(async (match) => {
-                    //Check children
-                    let T1C1 = (await ChildRepo.findOne({
-                        where: [{ id: match.team1_child1_id }],
-                        relations: ['illustrations', 'stories'],
-                    })) as Child;
-                    let T1C2 = (await ChildRepo.findOne({
-                        where: [{ id: match.team1_child2_id }],
-                        relations: ['illustrations', 'stories'],
-                    })) as Child;
-                    let T2C1 = (await ChildRepo.findOne({
-                        where: [{ id: match.team2_child1_id }],
-                        relations: ['illustrations', 'stories'],
-                    })) as Child;
-                    let T2C2 = (await ChildRepo.findOne({
-                        where: [{ id: match.team2_child2_id }],
-                        relations: ['illustrations', 'stories'],
-                    })) as Child;
-
-                    // We need to see if the child has any remaining votes. Child.votes < 3
-                    for (T1C1.votes; T1C1.votes < 3; T1C1.votes++) {
-                        // loop through child.votes until it equals 3
-                        //Get a random versus matchup that is NOT from this childs match
-                        let ChildMatch = await FindMatchByUID(T1C1.id, i.week);
-                        let response = await randomIgnoring(ChildMatch); // response is a versus matchup with lowest votes.
-                        let rand = Math.round(Math.random()); // random number, either 0 or 1
-                        console.log('response', response);
-                        if (response.story) {
-                            let Story = await StoryRepo.findOne({
-                                child: response.children[rand],
-                                week: i.week,
-                            });
-                            Story.votes++;
-                            await StoryRepo.save(Story);
-                        } else {
-                            // else it is an illustration
-                            let Illustration = await IllustrationRepo.findOne({
-                                child: response.children[rand],
-                                week: i.week,
-                            });
-                            Illustration.votes++;
-                            await IllustrationRepo.save(Illustration);
+                await VersusInCohort.forEach(async (vs) => {
+                    for (let j = 0; j < 3; j++) {
+                        if (vs.votes < 3) {
+                            let rand = Math.round(Math.random()); // random number, either 0 or 1
+                            if (vs.story) {
+                                let Story = await StoryRepo.findOne({
+                                    child: vs.children[rand],
+                                    week: i.week,
+                                });
+                                Story.votes++;
+                                await StoryRepo.save(Story);
+                            } else {
+                                // else it is an illustration
+                                let Illustration = await IllustrationRepo.findOne({
+                                    child: vs.children[rand],
+                                    week: i.week,
+                                });
+                                Illustration.votes++;
+                                await IllustrationRepo.save(Illustration);
+                            }
+                            vs.votes++;
+                            await VersusRepo.save(vs);
                         }
-
-                        response.votes++;
-                        await VersusRepo.save(response);
                     } // end for
-
-                    for (T1C2.votes; T1C2.votes < 3; T1C2.votes++) {
-                        // loop through child.votes until it equals 3
-                        //Get a random versus matchup that is NOT from this childs match
-                        let ChildMatch = await FindMatchByUID(T1C2.id, i.week);
-                        let response = await randomIgnoring(ChildMatch); // response is a versus matchup with lowest votes.
-                        let rand = Math.round(Math.random()); // random number, either 0 or 1
-
-                        if (response.story) {
-                            let Story = await StoryRepo.findOne({
-                                child: response.children[rand],
-                                week: i.week,
-                            });
-                            Story.votes++;
-                            await StoryRepo.save(Story);
-                        } else {
-                            // else it is an illustration
-                            let Illustration = await IllustrationRepo.findOne({
-                                child: response.children[rand],
-                                week: i.week,
-                            });
-                            Illustration.votes++;
-                            await IllustrationRepo.save(Illustration);
-                        }
-
-                        response.votes++;
-                        await VersusRepo.save(response);
-                    } // end for
-
-                    for (T2C1.votes; T2C1.votes < 3; T2C1.votes++) {
-                        // loop through child.votes until it equals 3
-                        //Get a random versus matchup that is NOT from this childs match
-                        let ChildMatch = await FindMatchByUID(T2C1.id, i.week);
-                        let response = await randomIgnoring(ChildMatch); // response is a versus matchup with lowest votes.
-                        let rand = Math.round(Math.random()); // random number, either 0 or 1
-
-                        if (response.story) {
-                            let Story = await StoryRepo.findOne({
-                                child: response.children[rand],
-                                week: i.week,
-                            });
-                            Story.votes++;
-                            await StoryRepo.save(Story);
-                        } else {
-                            // else it is an illustration
-                            let Illustration = await IllustrationRepo.findOne({
-                                child: response.children[rand],
-                                week: i.week,
-                            });
-                            Illustration.votes++;
-                            await IllustrationRepo.save(Illustration);
-                        }
-
-                        response.votes++;
-                        await VersusRepo.save(response);
-                    } // end for
-
-                    for (T2C2.votes; T2C2.votes < 3; T2C2.votes++) {
-                        // loop through child.votes until it equals 3
-                        //Get a random versus matchup that is NOT from this childs match
-                        let ChildMatch = await FindMatchByUID(T2C2.id, i.week);
-                        let response = await randomIgnoring(ChildMatch); // response is a versus matchup with lowest votes.
-                        let rand = Math.round(Math.random()); // random number, either 0 or 1
-
-                        if (response.story) {
-                            let Story = await StoryRepo.findOne({
-                                child: response.children[rand],
-                                week: i.week,
-                            });
-                            Story.votes++;
-                            await StoryRepo.save(Story);
-                        } else {
-                            // else it is an illustration
-                            let Illustration = await IllustrationRepo.findOne({
-                                child: response.children[rand],
-                                week: i.week,
-                            });
-                            Illustration.votes++;
-                            await IllustrationRepo.save(Illustration);
-                        }
-
-                        response.votes++;
-                        await VersusRepo.save(response);
-                    } // end for
-
-                    T1C1.progress.randomReview = T1C2.progress.randomReview = T2C1.progress.randomReview = T2C2.progress.randomReview = true;
-
-                    await ChildRepo.save([T1C1, T1C2, T2C1, T2C2]);
                 });
 
                 //Set teamReview & save
+                i.children.forEach(async (kid) => {
+                    kid.votes = 3;
+                    kid.progress.randomReview = true;
+                    await ChildRepo.save(kid);
+                });
+
                 i.activity = 'randomReview';
 
                 await CohortRepo.save(i);
             }
         });
-    }, 60000);
+    }, 30000);
 }
 
 function FindMatchesByChildren(allMatches, children) {
